185d27a6ec6e8789406fd840d5881e1dbe2ed80f
IvebbI <knurowskikarol12345@gmail.com>
2023-12-15 10:10:08 +0100

    Dodaj pliki projektów.

---
+++ MG_KK_Git/MG_KK_Git/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MG_KK_Git
+{
+    public partial class App : Application
+    {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "librus.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainTabbed());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MG_KK_Git/MG_KK_Git/Database.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MG_KK_Git
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+        public Database(string db)
+        {
+            _database = new SQLiteAsyncConnection(db);
+        }
+    }
+}
